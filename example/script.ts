c8296e2051c11fd0af6f6721e3a17a0bede790bf
shinyoshiaki <shinyoshiaki2012@gmail.com>
2021-12-01 19:47:50 +0900

    wip

---
+++ example/script.ts
@@ -0,0 +1,97 @@
+import Peer from "skyway-js";
+import { SkyWayRED } from "../src";
+
+(async function main() {
+  const localVideo = document.getElementById(
+    "js-local-stream"
+  ) as HTMLVideoElement;
+  const localId = document.getElementById("js-local-id");
+  const callTrigger = document.getElementById("js-call-trigger");
+  const closeTrigger = document.getElementById("js-close-trigger");
+  const remoteVideo = document.getElementById(
+    "js-remote-stream"
+  ) as HTMLVideoElement;
+  const remoteId = document.getElementById("js-remote-id") as HTMLInputElement;
+
+  const localStream = await navigator.mediaDevices.getUserMedia({
+    audio: true,
+    video: true,
+  });
+
+  // Render local stream
+  localVideo.muted = true;
+  localVideo.srcObject = localStream;
+  localVideo.playsInline = true;
+  await localVideo.play().catch(console.error);
+
+  const peer = ((window as any).peer = new Peer({
+    key: (window as any).__SKYWAY_KEY__,
+    config: {
+      //@ts-ignore
+      encodedInsertableStreams: true,
+    },
+    debug: 3,
+  }));
+  const skywayRED = new SkyWayRED({ useAdaptiveRedDistance: true });
+  setInterval(() => {
+    console.log(skywayRED.remoteRedDistance);
+  }, 1000);
+
+  // Register caller handler
+  callTrigger.addEventListener("click", () => {
+    // Note that you need to ensure the peer has connected to signaling server
+    // before using methods of peer instance.
+    if (!peer.open) {
+      return;
+    }
+
+    const mediaConnection = peer.call(remoteId.value, localStream);
+    skywayRED.activateRED(mediaConnection);
+
+    mediaConnection.on("stream", async (stream) => {
+      skywayRED.setupReceiver(mediaConnection);
+
+      // Render remote stream for caller
+      remoteVideo.srcObject = stream;
+      remoteVideo.playsInline = true;
+      await remoteVideo.play().catch(console.error);
+    });
+
+    mediaConnection.once("close", () => {
+      (remoteVideo.srcObject as MediaStream)
+        .getTracks()
+        .forEach((track) => track.stop());
+      remoteVideo.srcObject = null;
+    });
+
+    closeTrigger.addEventListener("click", () => mediaConnection.close(true));
+  });
+
+  peer.once("open", (id) => (localId.textContent = id));
+
+  // Register callee handler
+  peer.on("call", (mediaConnection) => {
+    mediaConnection.answer(localStream);
+    skywayRED.activateRED(mediaConnection);
+
+    mediaConnection.on("stream", async (stream) => {
+      skywayRED.setupReceiver(mediaConnection);
+
+      // Render remote stream for callee
+      remoteVideo.srcObject = stream;
+      remoteVideo.playsInline = true;
+      await remoteVideo.play().catch(console.error);
+    });
+
+    mediaConnection.once("close", () => {
+      (remoteVideo.srcObject as MediaStream)
+        .getTracks()
+        .forEach((track) => track.stop());
+      remoteVideo.srcObject = null;
+    });
+
+    closeTrigger.addEventListener("click", () => mediaConnection.close(true));
+  });
+
+  peer.on("error", console.error);
+})();
--- src/index.ts
+++ src/index.ts
@@ -1,84 +1,132 @@
 import { MediaConnection } from "skyway-js";
 import { buffer2ArrayBuffer, Red, RedSender } from "werift-rtp";
 
-function getRTCPeerConnection(connection: MediaConnection) {
-  connection.open = true;
-  const pc = connection.getPeerConnection();
-  connection.open = false;
-  return pc;
-}
+export class SkyWayRED {
+  private redSender: RedSender;
+  remoteRedDistance = 0;
+  readonly redDistance = this.options.redDistance ?? 1;
+  readonly useAdaptiveRedDistance = this.options.useAdaptiveRedDistance;
 
-export function activateRED(connection: MediaConnection) {
-  const pc = getRTCPeerConnection(connection);
-  const { codecs } = RTCRtpSender.getCapabilities("audio");
-  pc.getTransceivers()
-    .filter((t) => t.sender.track.kind === "audio")
-    .map((transceiver) => {
-      //@ts-ignore
-      transceiver.setCodecPreferences([
-        codecs.find((c) => c.mimeType.includes("red")),
-        ...codecs,
-      ]);
+  constructor(
+    private options: Partial<{
+      redDistance: number;
+      useAdaptiveRedDistance: boolean;
+    }> = {}
+  ) {}
+
+  private getRTCPeerConnection(connection: MediaConnection) {
+    connection.open = true;
+    const pc = connection.getPeerConnection();
+    connection.open = false;
+    return pc;
+  }
+
+  activateRED(connection: MediaConnection) {
+    const pc = this.getRTCPeerConnection(connection);
+    const { codecs } = RTCRtpSender.getCapabilities("audio");
+    pc.getTransceivers()
+      .filter((t) => t.sender.track.kind === "audio")
+      .map((transceiver) => {
+        //@ts-ignore
+        transceiver.setCodecPreferences([
+          codecs.find((c) => c.mimeType.includes("red")),
+          ...codecs,
+        ]);
+      });
+
+    let negotiated = false;
+    pc.addEventListener("connectionstatechange", () => {
+      if (pc.iceConnectionState === "connected" && !negotiated) {
+        negotiated = true;
+        pc.getSenders().forEach((sender) => {
+          if (sender.track.kind === "audio") {
+            const codec = sender
+              .getParameters()
+              .codecs.find((c) => c.mimeType.includes("red"));
+            const [blockPT] = codec.sdpFmtpLine.split("/");
+            this.redSender = new RedSender(Number(blockPT), this.redDistance);
+            this.senderTransform(sender, this.redSender);
+          } else {
+            this.senderTransform(sender);
+          }
+        });
+      }
     });
 
-  let negotiated = false;
-  pc.addEventListener("connectionstatechange", () => {
-    if (pc.iceConnectionState === "connected" && !negotiated) {
-      negotiated = true;
-      pc.getSenders().forEach((sender) => {
-        if (sender.track.kind === "audio") {
-          const codec = sender
-            .getParameters()
-            .codecs.find((c) => c.mimeType.includes("red"));
-          const [blockPT] = codec.sdpFmtpLine.split("/");
-          const redSender = new RedSender(Number(blockPT), 2);
-          senderTransform(sender, redSender);
-        } else {
-          senderTransform(sender);
+    if (this.useAdaptiveRedDistance) {
+      pc.addEventListener("connectionstatechange", () => {
+        if (pc.iceConnectionState === "disconnected") {
+          if (this.redSender && this.redSender.distance < 4) {
+            this.redSender.distance = 4;
+          }
+        }
+      });
+      new Promise<void>(async () => {
+        while (true) {
+          const stats = await pc.getStats();
+          const arr = [...(stats as any).values()];
+          const remoteInbound = arr.find((a) =>
+            a.id.includes("RTCRemoteInboundRtpAudioStream")
+          );
+          if (remoteInbound?.fractionLost) {
+            const distance = Math.round(remoteInbound.fractionLost * 10);
+            if (distance < this.redSender.distance) {
+              await new Promise((r) => setTimeout(r, 500));
+            }
+            this.redSender.distance = distance;
+            await new Promise((r) => setTimeout(r, 50));
+          }
         }
       });
     }
-  });
-}
+  }
 
-function senderTransform(sender: RTCRtpSender, redSender?: RedSender) {
-  //@ts-ignore
-  const senderStreams = sender.createEncodedStreams();
-  const readableStream = senderStreams.readable;
-  const writableStream = senderStreams.writable;
-  const transformStream = new TransformStream({
-    transform: (encodedFrame, controller) => {
-      if (sender.track.kind === "video") {
-        controller.enqueue(encodedFrame);
-        return;
-      }
-
-      const packet = Red.deSerialize(Buffer.from(encodedFrame.data));
-      const newPayload = packet.payloads.at(-1);
-      redSender.push({
-        buffer: newPayload.bin,
-        timestamp: encodedFrame.timestamp,
-      });
-      const red = redSender.build().serialize();
-      encodedFrame.data = buffer2ArrayBuffer(red);
-      controller.enqueue(encodedFrame);
-    },
-  });
-  readableStream.pipeThrough(transformStream).pipeTo(writableStream);
-}
-
-export function setupReceiver(connection) {
-  const pc = getRTCPeerConnection(connection);
-  pc.getReceivers().forEach((receiver) => {
+  private senderTransform(sender: RTCRtpSender, redSender?: RedSender) {
     //@ts-ignore
-    const receiverStreams = receiver.createEncodedStreams();
-    const readableStream = receiverStreams.readable;
-    const writableStream = receiverStreams.writable;
+    const senderStreams = sender.createEncodedStreams();
+    const readableStream = senderStreams.readable;
+    const writableStream = senderStreams.writable;
     const transformStream = new TransformStream({
       transform: (encodedFrame, controller) => {
+        if (sender.track.kind === "video") {
+          controller.enqueue(encodedFrame);
+          return;
+        }
+
+        const packet = Red.deSerialize(Buffer.from(encodedFrame.data));
+        const newPayload = packet.payloads.at(-1);
+        redSender.push({
+          buffer: newPayload.bin,
+          timestamp: encodedFrame.timestamp,
+        });
+        const red = redSender.build();
+        encodedFrame.data = buffer2ArrayBuffer(red.serialize());
         controller.enqueue(encodedFrame);
       },
     });
     readableStream.pipeThrough(transformStream).pipeTo(writableStream);
-  });
+  }
+
+  setupReceiver(connection) {
+    const pc = this.getRTCPeerConnection(connection);
+    pc.getReceivers().forEach((receiver) => {
+      //@ts-ignore
+      const receiverStreams = receiver.createEncodedStreams();
+      const readableStream = receiverStreams.readable;
+      const writableStream = receiverStreams.writable;
+      const transformStream = new TransformStream({
+        transform: (encodedFrame, controller) => {
+          if (
+            encodedFrame.data.byteLength > 0 &&
+            receiver.track.kind === "audio"
+          ) {
+            const red = Red.deSerialize(encodedFrame.data);
+            this.remoteRedDistance = red.payloads.length;
+          }
+          controller.enqueue(encodedFrame);
+        },
+      });
+      readableStream.pipeThrough(transformStream).pipeTo(writableStream);
+    });
+  }
 }
